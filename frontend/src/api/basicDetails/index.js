f6a7895d02e679d49435ca9643635f8cc022ecbe
Rashmi Chopade <Chopade_r@India.XoriantCorp.com>
2021-09-14 15:18:10 +0530

    api baseurl changes for multimodule different port number issue handling for backend

--- frontend/src/api/basicDetails/index.js
+++ frontend/src/api/basicDetails/index.js
@@ -1,19 +1,19 @@
 import axios from "axios"
-
+const host = 'http://localhost:7070/'
 const DoValidatePanNumberApi = panNumber =>{
-    return axios.post("PanNumber",panNumber);
+    return axios.post(host+"PanNumber",panNumber);
 }
 
 const DoValidateAadharNumberApi = aadharNumber =>{
-    return axios.post('AadharNumber',aadharNumber)
+    return axios.post(host+'AadharNumber',aadharNumber)
 }
 
 const DoValidateMobileNumberApi = mobileNumber =>{
-    return axios.post('MobileNumber',mobileNumber)
+    return axios.post(host+'MobileNumber',mobileNumber)
 }
 
 const DoSaveBasicDetailsApi = userData =>{
-    return axios.post('PanAadharMobile',userData)
+    return axios.post(host+'PanAadharMobile',userData)
 }
 const API =  {
     DoValidatePanNumberApi,
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authAPI from "./auth";
 import basicDetailsAPI from "./basicDetails"
-axios.defaults.baseURL = process.env.REACT_APP_BACKEND
+// axios.defaults.baseURL = process.env.REACT_APP_BACKEND
 //import and access apis with spread opertor
 const API = {
 ...authAPI,
