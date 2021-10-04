17f986de1102d65680d8624956dca82d17634695
Rashmi Chopade <Chopade_r@India.XoriantCorp.com>
2021-10-04 22:36:07 +0530

    drag and drop

--- frontend/src/components/FileUploader.js
+++ frontend/src/components/FileUploader.js
@@ -1,5 +1,6 @@
 import { Button, Grid, Box, makeStyles, Typography } from "@material-ui/core"
-import React, { useState } from "react"
+import React, { useState,useCallback }  from "react"
+import {useDropzone} from 'react-dropzone'
 const useStyles = makeStyles(theme => ({
     button: {
         borderRadius: 8,
@@ -21,41 +22,40 @@ const useStyles = makeStyles(theme => ({
 export default function FileUploader({ changeHandler, fileName, id, accept, fileSize, supportedFormats, maxFileSize,label }) {
     const [error, setError] = React.useState()
     const [fileSelectedName, setFileSelectedName] = React.useState(fileName)
+    const onDrop = useCallback(acceptedFiles => {
+        checkValidations(acceptedFiles[0])
+      }, [])
+      const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
     const fileChangeHandler = event => {
         if (event.target.files[0]) {
-            debugger
-            if (supportedFormats.includes(event.target.files[0] && event.target.files[0].type)) {
-                if (event.target.files[0] && event.target.files[0].size <= fileSize) {
-                    let file = event.target.files
-                    let fileName = event.target.files[0].name
-                    changeHandler(file, fileName)
-                    setError("")
-                    setFileSelectedName(fileName)
-                } else {
-                    // setError("Selected File : " + event.target.files[0] && event.target.files[0].name + " should be less than " + maxFileSize)
-                    setFileSelectedName(null)
-                    changeHandler(null, null,"Selected File : " + event.target.files[0] && event.target.files[0].name + " should be less than " + maxFileSize)
-                }
-            } else {
-                // setError("Selected File : " + event.target.files[0] && event.target.files[0].name + " is not supported. ")
-                setFileSelectedName(null)
-                changeHandler(null, null,"Selected File : " + event.target.files[0] && event.target.files[0].name + " is not supported. ")
-            }
+            checkValidations(event.target.files[0])
         }
     }
 
+    const checkValidations =(file)=>{
+        if (supportedFormats.includes(file.type)) {
+            if (file && file.size <= fileSize) {
+                // let file = file
+                let fileName = file.name
+                changeHandler(file, fileName)
+                setError("")
+                setFileSelectedName(fileName)
+            } else {
+                // setError("Selected File : " + event.target.files[0] && event.target.files[0].name + " should be less than " + maxFileSize)
+                setFileSelectedName(null)
+                changeHandler(null, null,"Selected File : " + file && file.name + " should be less than " + maxFileSize)
+            }
+        } else {
+            // setError("Selected File : " + event.target.files[0] && event.target.files[0].name + " is not supported. ")
+            setFileSelectedName(null)
+            changeHandler(null, null,"Selected File : " + file && file.name + " is not supported. ")
+        }
+    }
     const classes = useStyles();
     let file = null;
     return (
         <form>
             <Grid container xs={12} justifyContent="center">
-                {/* <Grid item xs={6}>
-                    {
-                        (fileSelectedName != null)
-                            ? (<span>File Selected - {fileSelectedName}</span>)
-                            : (<span>Choose a file... </span>)
-                    }
-                </Grid> */}
                 <Grid item xs={9} style={{}}>
                     <input
                         style={{ display: "none" }}
@@ -66,7 +66,14 @@ export default function FileUploader({ changeHandler, fileName, id, accept, file
                     <label htmlFor={"contained-button-file" + id}>
                         <div className={classes.dragAndDrop} >
                             <div className={classes.container}>
-                                <Typography align={"center"}>{label}</Typography>
+                            <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <Typography align={"center"}>Drop the file here ...</Typography> :
+          <Typography align={"center"}>{label}</Typography>
+      }
+    </div>
                                 <Typography align={"center"}>{"or"}</Typography>
                                 <Typography align={"center"}><Button className={classes.button} variant="contained" color={"primary"} component="span">Browse on your device</Button></Typography>
                             </div>
