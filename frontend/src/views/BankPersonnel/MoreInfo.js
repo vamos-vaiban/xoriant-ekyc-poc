25d4670a5975c3e8946ade820c1350eb59593da8
Rashmi Chopade <rashmi.chopade@xoriant.com>
2021-12-30 15:21:55 +0530

    Changes in styles

--- frontend/src/views/BankPersonnel/MoreInfo.js
+++ frontend/src/views/BankPersonnel/MoreInfo.js
@@ -13,42 +13,46 @@ export default function MoreInfo({ open, handleClose, userSpecificData }, props)
                 onClose={handleClose}
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
+                style={{height:"60%",width:"60%",marginLeft:"20%",marginRight:"20%",marginTop:"5%",marginBottom:"5%"}}
             >
                 <Paper>
                     <Box style={{ padding: "5%" }}>
                         <Typography variant={"h4"}>{"Requested Information"}</Typography>
-                        <Button style={{ float: "right" }}>{"X"}</Button>
+                        <Button style={{ float: "right" }} onClick={handleClose}>{"X"}</Button>
                         <Grid container spacing={2} style={{ padding: "5%" }}>
                             <Grid item xs={4}>
                                 <Box>
                                     {userSpecificData && userSpecificData.userPicUrl ?
-                                        <img style={{ borderRadius: '50%' }} height={150} width={150} src={userSpecificData && userSpecificData.userPicUrl} />
-                                        : <><AccountBoxIcon style={{ height: "100%", width: "100%;" }} /></>}
+                                        <img style={{ borderRadius: '50%' }}  src={userSpecificData && userSpecificData.userPicUrl} />
+                                        : <><AccountBoxIcon sx={{ fontSize: 150 }} /></>}
                                 </Box>
                             </Grid>
                             <Grid item xs={8}>
                                 <Box>
-                                <Typography>{"Address : "}{userSpecificData && userSpecificData.house_no} {userSpecificData && userSpecificData.address_line_1} {userSpecificData && userSpecificData.address_line_2} {userSpecificData && userSpecificData.landmark}, {userSpecificData && userSpecificData.city}</Typography>
-                                        <Typography>{"Mob No : "}{userSpecificData && userSpecificData.mobileNumber}</Typography>
+                                    <Typography>{"Address : "}{userSpecificData && userSpecificData.house_no} {userSpecificData && userSpecificData.address_line_1} {userSpecificData && userSpecificData.address_line_2} {userSpecificData && userSpecificData.landmark}, {userSpecificData && userSpecificData.city}</Typography>
+                                    <Typography>{"Mob No : "}{userSpecificData && userSpecificData.mobileNumber}</Typography>
 
-                                <Typography>{"Account Number : "}{userSpecificData && userSpecificData.accountNumber}</Typography>
-                                        <Typography>{"CRN number : "}{userSpecificData && userSpecificData.crnNumber}</Typography>
-                                        <Typography>{"Request Id : "}{userSpecificData && userSpecificData.reqId}</Typography>
+                                    <Typography>{"Account Number : "}{userSpecificData && userSpecificData.accountNumber}</Typography>
+                                    <Typography>{"CRN number : "}{userSpecificData && userSpecificData.crnNumber}</Typography>
+                                    <Typography>{"Request Id : "}{userSpecificData && userSpecificData.reqId}</Typography>
                                 </Box>
                             </Grid>
                             <Grid item xs={12}>
                                 <Box>
-                                <Typography>{"Validated Adhar Number : "}{userSpecificData && userSpecificData.adharNumber}</Typography>
-                                        <Typography>{"Validated Pan Number : "}{userSpecificData && userSpecificData.panNumber}</Typography>
-                                         <Typography>{"Similarity Percentage : "}{userSpecificData && userSpecificData.similarity}</Typography>
+                                    <Typography>{"Validated Adhar Number : "}{userSpecificData && userSpecificData.adharNumber}</Typography>
+                                    <Typography>{"Validated Pan Number : "}{userSpecificData && userSpecificData.panNumber}</Typography>
+                                    <Typography>{"Similarity Percentage : "}{userSpecificData && userSpecificData.similarity}</Typography>
                                 </Box>
                             </Grid>
                             <Grid item xs={12}>
                                 <Box>
-                               Document Uploaded</Box>
+                                    Document Uploaded</Box>
                             </Grid>
                             <Grid item xs={8}>
-                                <Box>xs=8</Box>
+                                <Box style={{marginLeft:"40%",marginRight:"20%"}}>
+                                    <Button variant='outlined' style={{marginRight:"20%"}}>{"Accept"}</Button>
+                                    <Button variant='outlined'>{"Reject"}</Button>
+                                </Box>
                             </Grid>
                         </Grid>
                     </Box>
