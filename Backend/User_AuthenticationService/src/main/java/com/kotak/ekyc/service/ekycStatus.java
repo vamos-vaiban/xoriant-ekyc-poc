9d945db32ecfab52fba8c343f09d8e504ab433c3
Rashmi Chopade <rashmi.chopade@xoriant.com>
2022-01-18 18:00:42 +0530

    deleted backend folders

--- Backend/User_AuthenticationService/src/main/java/com/kotak/ekyc/service/ekycStatus.java
+++
@@ -1,7 +0,0 @@
-package com.kotak.ekyc.service;
-
-import com.kotak.ekyc.model.ekycStatusModel;
-
-public interface ekycStatus {
-	public void save(ekycStatusModel kycstatus);
-}
--- BackendNew/AddressService/src/main/java/com/kotak/ekyc/service/AddressService.java
+++
@@ -1,9 +0,0 @@
-package com.kotak.ekyc.service;
-
-import com.kotak.ekyc.model.AddressModel;
-
-public interface AddressService {
-	
-	public AddressModel addressDetails(AddressModel addressModel);
-
-}
