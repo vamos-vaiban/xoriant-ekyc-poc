054e8c5aded344e02b5bef6498593b59bc2aedda
Amit Mittal <amit.mittal@xoriant.com>
2022-02-16 16:38:29 +0530

    added some properties related toencoded password

---
+++ ModelService/src/main/java/com/kotak/ekyc/model/LoginResponse.java
@@ -0,0 +1,31 @@
+package com.kotak.ekyc.model;
+
+public class LoginResponse {
+
+    private boolean status;
+    private String message;
+
+    public LoginResponse(boolean status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LoginResponse() {
+    }
+}
